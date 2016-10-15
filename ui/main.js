39c23121d767d74b4e4d85b60ef9915cbe5b1ea7
padhyevandan <padhyevandan@gmail.com>
2016-10-15 13:40:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,7 @@ console.log('Loaded!');
 
 var img = document.getElementById('mypic');
 
+var marginLeft = 0;
 function moveRight(){
     marginLeft = marginLeft + 5;
     img.style.marginLeft = marginLeft + 'px';
