d93c2d70159679f171a963449046dfeceb66a1bf
padhyevandan <padhyevandan@gmail.com>
2016-10-15 13:39:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,14 @@
 console.log('Loaded!');
+//move image
+
+var img = document.getElementById('mypic');
+
+function moveRight(){
+    marginLeft = marginLeft + 5;
+    img.style.marginLeft = marginLeft + 'px';
+}
+
+img.onClick = function(){
+    var interval = setInterval(moveRight,50);
+    
+};
