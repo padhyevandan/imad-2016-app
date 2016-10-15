b61bbffd708641645733588dc98c28a79a32d434
padhyevandan <padhyevandan@gmail.com>
2016-10-15 13:50:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,6 +10,6 @@ function moveRight(){
 }
 
 img.onClick = function(){
-    var interval = setInterval(moveRight,50);
-    
+    //var interval = setInterval(moveRight,50);
+    img.style.marginLeft='100px';
 };
